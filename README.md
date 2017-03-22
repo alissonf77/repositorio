ef4a857c4ad2e024659593a30b1e98f9151ca2e9
root <root@jessie.raw>
2017-03-22 16:26:33 +0000

    Primeira atualizacao consciente pra caramba!

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # repositorio
 repositorio para programacao
 repositorio para aula do dia 24/03/2017
+sinto-me violado.
+Meio chapadão.
---
+++ script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+USUARIO=alisson
+SENHA=123
+SAIR="NAO"
+while [ $SAIR != SAIR ]; do
+      clear
+     echo 
+     echo "Debian GNU/Linux 8 jessie tty1"
+     echo
+     QNT=1
+     while (( $QNT <= 5 )); do
+     echo -n "jessie login:"
+     read LOGIN
+     echo -n "Password:"
+     read PASSW
+     echo
+        if [ $LOGIN  == $USUARIO ]; then
+              if [ $PASSW == $SENHA ]; then
+               QNT=6
+               SAIR="SAIR"
+          else
+          sleep 1
+          echo
+          echo "Login incorrect"  
+              
+        fi    
+         else
+         sleep 1
+         echo 
+         echo "Login incorrect"
+       fi    
+       let QNT=($QNT+1)
+ 
+     done
+done
+
